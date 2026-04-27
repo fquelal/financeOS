d2662ebb85de9eb15c861cc5563ee5afb3abc80a
fquelal <x.quelal@gmail.com>
2026-04-26 22:06:19 -0400

    Update cache version to financeos-v1.6.4

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: Network first, cache fallback
 // Scope: /FinanceOS/
 
-const CACHE_NAME = 'financeos-v1.6.3';
+const CACHE_NAME = 'financeos-v1.6.4';
 const CACHED_URLS = [
   '/FinanceOS/',
   '/FinanceOS/index.html',
