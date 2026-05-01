8642a4d8864841fc3d2f41f50f962a8fd5a27a35
fquelal <x.quelal@gmail.com>
2026-05-01 08:30:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: Network first, cache fallback
 // Scope: /FinanceOS/
 
-const CACHE_NAME = 'financeos-v1.9.8';
+const CACHE_NAME = 'financeos-v1.9.9';
 const CACHED_URLS = [
   '/FinanceOS/',
   '/FinanceOS/index.html',
